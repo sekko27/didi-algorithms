bf1ff7f8099cda0c8bda2cc644a225881b5f793e
Balazs <sekko272727@gmail.com>
2020-08-04 08:35:53 +0200

    Use simple array as queue for internal representation of kahn graph.

--- lib/KahnGraph.ts
+++ lib/KahnGraph.ts
@@ -2,22 +2,35 @@ import { IEntity } from "./IEntity.ts";
 import { CyclicGraphError } from "./CyclicGraphError.ts";
 
 export class KahnGraph<N extends IEntity> {
-    private index: number = -1;
-    private readonly nodeIdToIndexMap: Map<string, number> = new Map();
-    private readonly indexToNodeMap: N[] = [];
-    private readonly inDegrees: number[] = [];
-    private readonly internal: Set<number>[] = [];
+    /**
+     * Hash by node identifier to index.
+     */
+    readonly #nodeIdToIndexMap: Map<string, number> = new Map();
+
+    /**
+     * Reverse mapping from index to node representation.
+     */
+    readonly #indexToNodeMap: N[] = [];
+
+    /**
+     * Stores the node in-degrees by index.
+     */
+    readonly #inDegrees: number[] = [];
+
+    /**
+     * Stores the set of node indexes the current node connected to.
+     */
+    readonly #internal: Set<number>[] = [];
 
     private registerNode(node: N): number {
-        if (!this.nodeIdToIndexMap.has(node.id)) {
-            const index = ++this.index;
-            this.nodeIdToIndexMap.set(node.id, index);
-            this.indexToNodeMap.push(node);
-            this.internal.push(new Set());
-            this.inDegrees.push(0);
+       if (!this.#nodeIdToIndexMap.has(node.id)) {
+            const index = this.#indexToNodeMap.push(node) - 1;
+            this.#nodeIdToIndexMap.set(node.id, index);
+            this.#internal.push(new Set());
+            this.#inDegrees.push(0);
             return index;
         } else {
-            return this.nodeIdToIndexMap.get(node.id) as number;
+            return this.#nodeIdToIndexMap.get(node.id) as number;
         }
     }
 
@@ -29,29 +42,28 @@ export class KahnGraph<N extends IEntity> {
     public addEdge(from: N, to: N): this {
         const fromIndex = this.registerNode(from);
         const toIndex = this.registerNode(to);
-        this.internal[fromIndex].add(toIndex);
-        this.inDegrees[toIndex]++;
+        this.#internal[fromIndex].add(toIndex);
+        this.#inDegrees[toIndex]++;
         return this;
     }
 
     public sort(): N[] {
         // Clone the in-degrees
-        const inDegrees: number[] = [ ...this.inDegrees ];
+        const inDegrees: number[] = [ ...this.#inDegrees ];
 
-        const queue: Set<number> = new Set();
+        const queue: number[] = [];
         const result: N[] = [];
         inDegrees.forEach((id, index) => {
             if (id === 0) {
-                queue.add(index);
+                queue.push(index);
             }
         });
-        while (queue.size > 0) {
-            const currentIndex = queue.keys().next().value;
-            queue.delete(currentIndex);
-            result.push(this.indexToNodeMap[currentIndex]);
-            for (const neighbour of this.internal[currentIndex]) {
+        while (queue.length > 0) {
+           const currentIndex = queue.shift() as number;
+           result.push(this.#indexToNodeMap[currentIndex]);
+            for (const neighbour of this.#internal[currentIndex]) {
                 if ((--inDegrees[neighbour]) === 0) {
-                    queue.add(neighbour);
+                    queue.push(neighbour);
                 }
             }
         }
@@ -65,6 +77,6 @@ export class KahnGraph<N extends IEntity> {
         return inDegrees
             .map((id, index) => [id, index])
             .filter(([id]) => id > 0)
-            .map(([_, index]) => this.indexToNodeMap[index])
+            .map(([_, index]) => this.#indexToNodeMap[index])
     }
 }
